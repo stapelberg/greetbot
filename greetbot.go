82cde7738ceafb6c216e03e248c84a7996466965
Michael Stapelberg <michael@stapelberg.de>
2012-02-11 16:26:35 +0000

    Initial commit

---
+++ greetbot.go
@@ -0,0 +1,139 @@
+// vim:ts=4:sw=4
+// i3 - IRC bot to greet people and tell them to wait
+// © 2012 Michael Stapelberg (see also: LICENSE)
+package main
+
+import irc "github.com/fluffle/goirc/client"
+import (
+	"log"
+	"flag"
+	"strings"
+	"time"
+	"math/rand"
+	"greetbot/histogram"
+)
+
+var irc_channel *string = flag.String(
+	"channel",
+	"#i3-build",
+	"In which channel this bot should be in")
+
+var greetings *string = flag.String(
+	"greetings",
+	"hello,hallo,hey,hi,yo,good morning,ohai",
+	"Greeting words on which the bot should react, comma-separated")
+
+var histogram_path *string = flag.String(
+	"histogram_path",
+	"./histogram.data",
+	"Serialized protobuf file containing the histogram data")
+
+var my_histogram histogram.Histogram = histogram.Load(*histogram_path)
+
+var lastGreetingTime time.Time = time.Now()
+
+// Returns true if any of the greeting words of the -greetings flag is
+// contained in the given message.
+func containsGreeting(msg string) bool {
+	lcMsg := strings.ToLower(msg)
+	for _, greetword := range strings.SplitN(*greetings, ",", -1) {
+		if strings.Contains(lcMsg, greetword) {
+			return true
+		}
+	}
+	return false
+}
+
+func isBot(nickname string) bool {
+	// TODO
+	return false
+}
+
+// Called when somebody writes some message to the IRC channel.
+func handleMessage(conn *irc.Conn, line *irc.Line) {
+	msg := line.Args[1]
+	if line.Args[0] != *irc_channel {
+		log.Printf(`Ignoring private message to me: "%s"`, msg)
+		return
+	}
+
+	log.Printf("Received: *%s* says: \n", line.Nick)
+
+	// Ignore messages from bots.
+	if isBot(line.Nick) {
+		log.Printf("Ignoring message, %s is a bot", line.Nick)
+		return
+	}
+
+	// Count every line as activity.
+	my_histogram.LogActivity(line.Nick)
+
+	// Check for greetings and say hello.
+	if len(msg) < 10 && containsGreeting(msg) {
+		if (time.Now().Unix() - lastGreetingTime.Unix()) < 30 {
+			log.Printf("Not replying, 30 seconds have not passed yet")
+			return
+		}
+		log.Printf(`Replying to line "%s"`, msg)
+		lastGreetingTime = time.Now()
+		go func() {
+			// Reply after a random time, at least 1s after the
+			// original line, at most 5s after the original line.
+			fuzz := rand.Int63n(4000)
+			time.Sleep((time.Duration)(1000 + fuzz) * time.Millisecond)
+			conn.Privmsg(*irc_channel, "hello!")
+		}()
+		return
+	}
+
+	// Otherwise, see if that was a question.
+	if !strings.HasSuffix(strings.TrimSpace(msg), "?") {
+		return
+	}
+
+	log.Printf(`Got question "%s" from "%s"`, msg, line.Nick)
+
+	// See if we have enough data points about the activity of this channel for
+	// this day and hour.
+	if my_histogram.IsActive() {
+		return
+	}
+
+	log.Printf("Telling user to be patient\n")
+}
+
+func main() {
+	flag.Parse()
+
+	quit := make(chan bool)
+
+	c := irc.SimpleClient("Eyo", "i3", "http://i3wm.org/")
+
+	c.AddHandler("connected",
+		func(conn *irc.Conn, line *irc.Line) {
+			log.Printf("Connected, joining channel %s\n", *irc_channel)
+			conn.Join(*irc_channel)
+		})
+
+	c.AddHandler("disconnected",
+		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+	c.AddHandler("PRIVMSG", handleMessage)
+
+	log.Printf("Connecting...\n")
+	if err := c.Connect("irc.twice-irc.de"); err != nil {
+		log.Printf("Connection error: %s\n", err.Error())
+	}
+
+	// program main loop
+	for {
+		select {
+		case <-quit:
+			log.Println("Disconnected. Reconnecting...")
+			if err := c.Connect("irc.twice-irc.de"); err != nil {
+				log.Printf("Connection error: %s\n", err.Error())
+			}
+		}
+	}
+	log.Fatalln("Fell out of the main loop?!")
+}
---
+++ histogram/histogram.go
@@ -0,0 +1,132 @@
+// vim:ts=4:sw=4
+// i3 - IRC bot to greet people and tell them to wait
+// © 2012 Michael Stapelberg (see also: LICENSE)
+package histogram
+
+import (
+	"fmt"
+	"log"
+	"encoding/gob"
+	"time"
+	"os"
+	"strings"
+)
+
+// A combination of time.Weekday and an integer representing each hour of the
+// day. This type is used as the key for our histogram.
+type WeekdayHour struct {
+	Weekday time.Weekday
+	Hour int
+}
+
+type histogramEntry struct {
+	// The average delay between two messages, in seconds.
+	AverageDelayBetweenMessages int64
+
+	// The number of different IRC nicknames counted during this particular
+	// hour.
+	NumberOfNicknames int
+}
+
+type currentStats struct {
+	// Stores all the different nicknames.
+	nicknames map[string]bool
+
+	// The following two fields form the average delay between messages, which
+	// roughly indicates how active the channel is.
+	delayBetweenMessages int64
+	numberOfMessages int64
+	lastMessage time.Time
+
+	// The weekday/hour this struct is for. If the current weekday/hour doesn’t
+	// match this one anymore, we need to store this struct and begin a new
+	// one.
+	weekdayHour WeekdayHour
+}
+
+type Histogram struct {
+	filename string
+	current currentStats
+	Histogram map[WeekdayHour]histogramEntry
+}
+
+func (h *Histogram) IsActive() bool {
+	fmt.Printf("Checking if the channel is currently inactive.\n")
+	// TODO: we probably want the median here, then compare the current and last-hour values
+	return true
+}
+
+func (h *Histogram) LogActivity(nickname string) {
+	now := time.Now().UTC()
+	key := WeekdayHour{now.Weekday(), now.Hour()}
+	if h.current.weekdayHour != key {
+		fmt.Printf("Rotating, current = %s, key = %s\n", h.current.weekdayHour, key)
+		entry := h.Histogram[key]
+		if h.current.numberOfMessages > 0 {
+			entry.AverageDelayBetweenMessages =
+				(h.current.delayBetweenMessages / h.current.numberOfMessages)
+		} else {
+			entry.AverageDelayBetweenMessages = -1
+		}
+		entry.NumberOfNicknames = len(h.current.nicknames)
+		h.Histogram[key] = entry
+
+		h.current.delayBetweenMessages = 0
+		h.current.numberOfMessages = 0
+		h.current.nicknames = make(map[string]bool, 100)
+		h.current.weekdayHour = key
+	}
+
+	h.current.delayBetweenMessages += (now.Unix() - h.current.lastMessage.Unix())
+	h.current.numberOfMessages += 1
+	h.current.nicknames[strings.ToLower(nickname)] = true
+
+	fmt.Println(h)
+
+	// Write the stats file to disk. We might not want to do this on every change.
+	file, err := os.OpenFile(h.filename, os.O_WRONLY | os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatal(`Could not open histogram file "%s": %s`, h.filename, err)
+	}
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	if err := encoder.Encode(h); err != nil {
+		log.Fatal(`Could not write histogram data to "%s": %s`, h.filename, err)
+	}
+}
+
+func Load(filename string) Histogram {
+	now := time.Now().UTC()
+	var result Histogram
+	result.filename = filename
+	result.Histogram = make(map[WeekdayHour]histogramEntry, 0)
+
+	// Check if the file exists and load it, if so. Otherwise, create a new file.
+	if _, err := os.Stat(filename); err != nil {
+		if e, ok := err.(*os.PathError); ok && e.Err != os.ENOENT {
+			log.Fatalf(`Error loading histogram data from "%s": %s`, filename, err)
+		} 
+
+		// Err == os.ENOENT, this is ok.
+	} else {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf(`Error reading histogram data from "%s": %s`, filename, err)
+		}
+		defer file.Close()
+
+		decoder := gob.NewDecoder(file)
+		if err := decoder.Decode(&result); err != nil {
+			log.Fatalf(`Could not load histogram from "%s": %s`, filename, err)
+		}
+	}
+
+	// Initialize currentStats, no matter whether we loaded a previous state
+	// file or not.
+	result.current.nicknames = make(map[string]bool, 100)
+	result.current.weekdayHour = WeekdayHour{now.Weekday(), now.Hour()}
+	result.current.lastMessage = now
+
+	return result
+}
